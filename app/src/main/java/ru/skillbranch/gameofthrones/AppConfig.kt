aafa3f243b2b824baab16111b77fabc9a35036ff
Vladimir <vkomarov@baccasoft.ru>
2020-07-12 10:29:24 +0400

    WIP добавил исходники

--- app/src/main/java/ru/skillbranch/gameofthrones/AppConfig.kt
+++ app/src/main/java/ru/skillbranch/gameofthrones/AppConfig.kt
@@ -1,8 +1,15 @@
 package ru.skillbranch.gameofthrones
 
 object AppConfig {
-
-    const val BASE_URL = ""
     const val RETROFIT_CONNECTION_TIMEOUT_SEC = 20L
-
+    val NEED_HOUSES = arrayOf(
+        "House Stark of Winterfell",
+        "House Lannister of Casterly Rock",
+        "House Targaryen of King's Landing",
+        "House Greyjoy of Pyke",
+        "House Tyrell of Highgarden",
+        "House Baratheon of Dragonstone",
+        "House Nymeros Martell of Sunspear"
+    )
+    const val BASE_URL = "https://www.anapioficeandfire.com/api/"
 }
